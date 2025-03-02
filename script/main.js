70b5a7f6d9b9c728f72a764b4627c8ba5810b797
Tushar Chakravarti <tusarchakravarti@gmail.com>
2025-03-02 23:00:56 +0600

    Almost done

---
+++ script/main.js
@@ -0,0 +1,21 @@
+document.getElementById("blog").addEventListener("click", function (event) {
+  window.location.href = "./blog.html";
+});
+
+
+
+/**
+ * Back To Desk
+ */
+document.getElementById("back-btn").addEventListener("click", function(event){
+    // alert("Clicked")
+    window.location.href = "main.html";
+})
+
+const colorButton = document.getElementById('colorChange');
+const colors = ['#FF5733', '#FF5721', '#33FF57', '#FF33A1', '#33FFF5'];
+
+colorButton.addEventListener('click', function(event) {
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+    document.body.style.backgroundColor = randomColor;
+});
